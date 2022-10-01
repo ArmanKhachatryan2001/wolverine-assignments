1d8eae47990545b7636ae876b6722c3c07a761cc
ArmanKhachatryan2001 <104670251+ArmanKhachatryan2001@users.noreply.github.com>
2022-10-01 11:50:44 +0400

    Update replaceAll.cpp

--- sep27/replaceAll.cpp
+++ sep27/replaceAll.cpp
@@ -12,9 +12,27 @@ const std::string replaceAll(const std::string& s,const std::string& s1,const st
     }
     return tmp;
 }
+/*
+const std::string replaceAll(std::string s,const std::string& s1,const std::string& s2)
+{
+    int x = 0;
+    for (int i = 0; i < s.size(); ++i) {
+        if (s.find(s1) != -1) {
+           int x = s.find(s1);
+           s.erase(x, s1.size());
+           s.insert(x, s2);
+        }
+    }
+    return s;
+}
 
-
-
+int main() {
+    std::string str = "hello";
+    std::string str1 = "hello";
+    std::string str2 = "world";
+    std::cout << replaceAll(str, str1, str2);
+}
+*/
 int main() {
     std::string str = "lhelloworldl";
     std::string str1 = "l";
